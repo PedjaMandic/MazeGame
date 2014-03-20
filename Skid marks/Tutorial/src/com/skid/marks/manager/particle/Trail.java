b8cc8b99a54452b1e7ca845563c0e685617bb748
Fredrik <f.wigarde@hotmail.com>
2014-03-20 21:35:42 +0100

    Kommenterad trail

--- Skid marks/Tutorial/src/com/skid/marks/manager/particle/Trail.java
+++ Skid marks/Tutorial/src/com/skid/marks/manager/particle/Trail.java
@@ -30,40 +30,53 @@ public class Trail implements BaseParticle{
 		Init();
 	}
 	
-	private void Init(){
-		
+	/*
+	 * Initializes the main components of the trail.
+	 * Loads in the correct texture to the sprite.
+	 */
+	private void Init(){		
+		//Load texture
 		sprite = game.Textures.getSprite("data/gfx/particle.png");
-		sprite.setOrigin(0, sprite.getHeight() / 2);
-//		sprite.rotate(rotation);
 		
+		//Set the rotation origin and the rotation angle.
+		sprite.setOrigin(0, sprite.getHeight() / 2);
+		//sprite.rotate(rotation);		TODO Decide on this
+		
+		//Sets x position, height and width.
 		positionX = Gdx.graphics.getWidth()/6f;
+		WIDTH = Gdx.graphics.getWidth()/64;	
 		HEIGHT = Gdx.graphics.getHeight()/16;
+		
+		//Reduces height by 4 and center it to the player
 		HEIGHT -= 4;
 		positionY += 2;
 		
-		WIDTH = 20;	
-		alpha = 100;
-		
-		color = new Color(255, 0, 0, alpha);
-		
-		
+		//Sets starting alpha value
+		alpha = 100;		
+			
 	}
 
 	@Override
 	public void update(float delta) {
-		positionX -= WIDTH;
-//		HEIGHT -=4;
-//		positionY += 2;
+		//Moves the trail backwards by it's width amount
+		positionX -= WIDTH+2;
+		
+		//Reduces the alpha value
 		alpha -= 7 ;
+		
+		//Gets the current color off the background and sets it to the color of the trail
 		Color t = Background.currentColor;
 		color = new Color(t.r, t.g, t.b, alpha/255f);
 	}
 
 	@Override
 	public void draw(SpriteBatch batch) {
+		//Sets the position, size and color of the trail
 		sprite.setPosition(positionX, positionY);
 		sprite.setSize(WIDTH, HEIGHT);
 		sprite.setColor(color);
+		
+		//Draws the trail
 		sprite.draw(batch);
 	}
 

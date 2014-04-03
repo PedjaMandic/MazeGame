04de337c9dbdef14953004a95ff5e7a8de6fc348
hubbas <hawezshwan@gmail.com>
2014-04-03 22:13:22 +0200

    Rensat och uppdaterat namn

---
+++ Tri Hard/Tutorial/src/com/trihard/manager/SoundManager.java
@@ -0,0 +1,99 @@
+package com.trihard.manager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+
+public class SoundManager {
+
+	private Map<String, Sound> sounds = new HashMap<String, Sound>();
+	private Map<String, Music> music = new HashMap<String, Music>();
+	
+	private boolean soundOn = true;
+	
+	public SoundManager() {
+		// Sounds
+		sounds.put("explosion", Gdx.audio.newSound(Gdx.files.internal("data/sfx/explosion.wav")));
+		
+		// Music
+		music.put("music", Gdx.audio.newMusic(Gdx.files.internal("data/sfx/level_music.mp3")));
+		music.put("menu", Gdx.audio.newMusic(Gdx.files.internal("data/sfx/menu_music.mp3")));
+	}
+	
+	public void dispose() {
+		if(sounds != null) {
+			for(Sound s : sounds.values()) {
+				s.dispose();
+			}
+			sounds.clear();
+			sounds = null;
+		}
+		if(music != null) {
+			for(Music m : music.values()) {
+				m.dispose();
+			}
+			music.clear();
+			music = null;
+		}
+	}
+	
+	/*
+	 * Spela upp ett ljud eller musik
+	 **/
+	public void play(String name) {
+		if(!soundOn)
+			return;
+		if(sounds.containsKey(name)) {
+			Sound s = sounds.get(name);
+			s.stop();
+			s.play(0.5f);
+		} else if(music.containsKey(name)) {
+			Music m = music.get(name);
+			m.setVolume(1.0f);
+			m.setLooping(true);
+			m.play();
+		}
+	}
+	
+	public void play(String name, boolean stopAll) {
+		if(stopAll) {
+			stopAll();
+		}
+		play(name);
+	}
+	
+	public void stop(String name) {
+		if(sounds.containsKey(name)) {
+			Sound s = sounds.get(name);
+			s.stop();
+		} else if(music.containsKey(name)) {
+			Music m = music.get(name);
+			m.stop();
+		}
+	}
+	
+	public void stopAll() {
+		for(Sound s : sounds.values()) {
+			s.stop();
+		}
+		for(Music m : music.values()) {
+			m.stop();
+		}
+	}
+	
+	public void setSound(boolean on) {
+		if(on) {
+			soundOn = true;
+		} else {
+			soundOn = false;
+			stopAll();
+		}
+	}
+	
+	public boolean isPlaying(String name) {
+		return music.get(name).isPlaying();
+	}
+}
---
+++ Tri Hard/Tutorial/src/com/trihard/screens/Play.java
@@ -0,0 +1,151 @@
+package com.trihard.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.trihard.game.Level;
+import com.trihard.game.Player;
+import com.trihard.game.TriHard;
+import com.trihard.manager.particle.Star;
+
+public class Play implements Screen, InputProcessor {
+
+	private final TriHard game;
+	
+	private Level level;
+	private Player player;
+	private float score = 0;
+	
+	private Sprite score_background;
+	
+	public Play(TriHard game) {
+		this.game = game;
+		Gdx.input.setInputProcessor(this);
+		Highscore.LoadPrefs();
+		
+		game.Particles.clear();
+		player = new Player(game);
+		player.init();
+
+		level = new Level(game);
+		
+		game.ingame_font.setColor(Color.WHITE);
+		
+		score_background = game.Textures.getSprite("data/gfx/menu/button.png");
+		score_background.setBounds(0, 0, Gdx.graphics.getWidth()/4, Gdx.graphics.getHeight()/6);
+		if(MainMenu.hasSound)
+			game.Sounds.play("music", true);
+	}
+	
+	@Override
+	public void dispose() {
+	}
+	
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(100/255f, 100/255f, 1.0f, 1.0f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		float time = Gdx.graphics.getDeltaTime();
+		
+		// UPDATE
+		if(!Level.isBetweenLevels)
+			score += delta*60;
+		game.Particles.add(new Star(game));
+		
+		player.update(time);
+		
+		level.update(time);
+		
+		if(level.HasCollided(player.getBounds()))
+		{
+			game.Sounds.play("explosion");
+			game.setScreen(new GameOver(game, level, player, (int)score));
+			
+			Highscore.InitHighscore();
+			isHighscore((int)score);
+		}
+		
+		// DRAW
+		game.Camera.update();
+		game.Batch.setProjectionMatrix(game.Camera.combined);
+		game.Batch.begin();
+		level.draw(game.Batch);
+		score_background.draw(game.Batch);
+		game.ingame_font.draw(game.Batch, String.format("Score: %d", (int)score), score_background.getWidth()*0.12f, score_background.getHeight()*0.2f);
+		game.ingame_font.draw(game.Batch, String.format("Level: %d", Level.currentLevel), score_background.getWidth()*0.12f, score_background.getHeight()*0.6f);
+		player.draw(game.Batch);
+		game.Batch.end();
+	}
+	
+	private void isHighscore(int newscore) {
+		int checker = Highscore.CheckScore(newscore);
+		
+		if(checker == 1) {
+			Highscore.SaveScore(1, newscore);
+		} else if(checker == 2) {
+			Highscore.SaveScore(2, newscore);
+		} else if(checker==3) {
+			Highscore.SaveScore(3, newscore);
+		}
+	}
+
+	@Override
+	public void resize(int width, int height) {}
+
+	@Override
+	public void show() {}
+
+	@Override
+	public void hide() {}
+
+	@Override
+	public void pause() {}
+
+	@Override
+	public void resume() {}
+	
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
+	}
+
+}

1f1c78ca1377852c539754c1bcd8d8cd7a79d37f
Orvar <orvarsmejl@hotmail.com>
2014-04-10 22:24:21 +0200

    Kommenterat
    På fancy pants engelska

--- Tri Hard/Tutorial/src/com/trihard/game/Level.java
+++ Tri Hard/Tutorial/src/com/trihard/game/Level.java
@@ -12,34 +12,34 @@ public class Level {
 	
 	private TriHard game;
 	
-	private Random random;
-	private Sprite sprite;
-	private float distanceSinceLastPoint = 0.0f;
-	private float distanceBetweenPoints; //half screen
-	private float currentPoint;
-	private float nextPoint;
-	private float tunnelWidth;
+	private Random random; //for randomisation
+	private Sprite sprite; //sprite for rows
+	private float distanceSinceLastPoint;
+	private float distanceBetweenPoints;
+	private float currentPoint; //the Y coordinate for the current point (0.0f is top, 1.0 is bottom)
+	private float nextPoint; //the Y coordinate for the next point (0.0f is top, 1.0 is bottom)
+	private float tunnelWidth; //the width of the tunnel (measured in pixel amounts)
 	
-	private Row rows[];
-	private int lowestRow;
-	private float levelSpeed;
-	private float rowHeight;
-	private int nrOfRows;
+	private Row rows[]; //the rows of the level
+	private int lowestRow; //the position of the leftmost row
+	private float levelSpeed; //the speed the level travels, measured in pixels per second
+	private float rowWidth; //the width of an individual row, measured in pixels
+	private int nrOfRows; //the total amount of rows for easy reference
 	
-	private float timeUntilLevelStarts;
-	private float totalPauseTime;
-	private float timeUntilLevelEnds;
-	private float timeBetweenLevels;
-	private float maximumPointDifference;
+	private float timeUntilLevelStarts; //the amount of time left for the pauses
+	private float totalPauseTime; //the total amount of time for pauses
+	private float timeUntilLevelEnds; //the amount of time left for the actual level
+	private float timeBetweenLevels; //the total amount of time for the level
+	private float maximumPointDifference; //determines the maximum difference a between points when randomly determining the next point (measured in percentages)
 	
-	public static int currentLevel = 0;
+	public static int currentLevel = 0; //the current level (0 being the first intermission, and 1, 2, 3 and so forth being actual levels)
 	
-	private Background background;
+	private Background background; //the moving background
 	
-	//för pausskärm
-	public static boolean isBetweenLevels;
-	private Sprite pauseSprite;
+	public static boolean isBetweenLevels; //boolean for determining if there is currently an intermission between levels
+	private Sprite pauseSprite; //sprite that holds the "New Level" text that flies by during intermission
 
+	//constructor
 	public Level(TriHard game)
 	{
 		this.game = game;
@@ -48,55 +48,55 @@ public class Level {
 		this.reset();
 	}
 	
+	//resets all values to their default. Is called when the player starts/restarts the game
 	public void reset() {
 		TriHard.screen_width = Gdx.graphics.getWidth();
 		TriHard.screen_height = Gdx.graphics.getHeight();
-		tunnelWidth = TriHard.screen_height*0.5f;
-		nrOfRows = 1+(int)TriHard.screen_width/40;
-		rowHeight = TriHard.screen_width / (nrOfRows-1);
-		distanceBetweenPoints = TriHard.screen_width/4;
+		tunnelWidth = TriHard.screen_height*0.5f; //default tunnelwidth is half a screen wide.
+		nrOfRows = 1+(int)TriHard.screen_width/40; //on a 1280x720 resolution, this results in 32 rows. The higher resolution, the more rows. The extra +1 is for smoothness
+		rowWidth = TriHard.screen_width / (nrOfRows-1);
+		distanceBetweenPoints = TriHard.screen_width/4; //nextpoint is determined after a fourth of a screen
 		levelSpeed = TriHard.screen_width * 0.8f;
-		currentPoint = 0.5f;
-		nextPoint = 0.5f;
-		isBetweenLevels = true;
+		currentPoint = 0.5f; //percentage of the games height where the current point is
+		nextPoint = 0.5f; //to avoid problems, the next point has the same percentage
+		isBetweenLevels = true; //the game starts in intermission
 		currentLevel = 0;
-		timeBetweenLevels = 20f;
+		timeBetweenLevels = 20f; //the first level is 20 seconds long, higher levels are longer
 		timeUntilLevelEnds = timeBetweenLevels;
-		totalPauseTime = 3f;
+		totalPauseTime = 3f; //the intermission time is always three seconds
 		timeUntilLevelStarts = totalPauseTime;
 		distanceSinceLastPoint = 0.0f;
-		maximumPointDifference = 0.4f;
+		maximumPointDifference = 0.4f; //in order to not have too much of a difficulty in the game, maximum change interval between points is 40%. This is never changed
 		
-		sprite = game.Textures.getSprite("data/gfx/rows_2.1.png");
-		pauseSprite = game.Textures.getSprite("data/gfx/newlevel2.png");
-		pauseSprite.setColor(Color.WHITE);
+		sprite = game.Textures.getSprite("data/gfx/rows_2.1.png"); //all the rows look the same, and thus share the same sprite
+		pauseSprite = game.Textures.getSprite("data/gfx/newlevel2.png"); //the pausetext needs it's own sprite
+		pauseSprite.setColor(Color.WHITE); 
 		
-
-
-		
-		//de rader som ritas ut
-		rows = new Row[nrOfRows];
+		rows = new Row[nrOfRows]; //the array that holds the rows
 		for(int i = 0; i < rows.length; i++)
 		{
-			rows[i] = new Row(TriHard.screen_height/2, tunnelWidth, (rows.length - (i-1))*rowHeight, !isBetweenLevels);
-
+			 //all the rows are set as a straight line as default. Not that it matters since it'll be part of the intermission
+			rows[i] = new Row(TriHard.screen_height/2, tunnelWidth, (rows.length - (i-1))*rowWidth, !isBetweenLevels);
 		}
-		lowestRow = nrOfRows-1;
+		lowestRow = nrOfRows-1; //the lowest row is last place in the array
 		
 	}
 	
-	//förbättra på något sätt
-	// Rect == player bounds
+	//collision-detection, that checks only against the rows that "contain" the same Y coordinates as the player
+	//rect is (usually) the player rectangle
 	public boolean HasCollided(Rectangle rect)
 	{
-		int start = (nrOfRows + lowestRow - (int)(rect.x/rowHeight)-1)%nrOfRows;
+		//this part determines which part of the row-array "contains" the player
+		int start = (nrOfRows + lowestRow - (int)(rect.x/rowWidth)-1)%nrOfRows;
 		int nrOfExtraRowsToCheck = 0;
 		
-		while(rows[(nrOfRows+start-nrOfExtraRowsToCheck)%nrOfRows].X+rowHeight < rect.x + rect.width)
+		//while this checks for additional rows to check due to the players size
+		while(rows[(nrOfRows+start-nrOfExtraRowsToCheck)%nrOfRows].X+rowWidth < rect.x + rect.width)
 		{
 			nrOfExtraRowsToCheck++;
 		}
 		
+		//no point in checking if the rows aren't visible or collidable
 		if(rows[start].active)
 		{
 			if(rect.y < rows[start].leftWidth)
@@ -105,6 +105,7 @@ public class Level {
 				return true;
 		}
 		
+		//uses modulus to determine which part of the array to check
 		for(int i = start-1; i >= start-nrOfExtraRowsToCheck; i--)
 		{
 			if(rows[(nrOfRows+i)%nrOfRows].active)
@@ -116,111 +117,127 @@ public class Level {
 			}
 		}
 		
+		//if nothing returned true, then obviously there was no collision
 		return false;
 	}
 	
 	public void dispose() {
 	}
 	
-	
+	//triggers once per intermission, when it ends
 	private void endPauseTrigger()
 	{
+		//increase in difficulty
 		if(currentLevel < 9){
-			currentLevel++;
-			tunnelWidth *= 0.95f;
-			levelSpeed *= 1.05f;
-			timeBetweenLevels *= 1.05f;
-			Player.MOVE_SPEED *= 1.05f;
+			currentLevel++; //a higher level
+			tunnelWidth *= 0.95f; //tighter tunnel
+			levelSpeed *= 1.05f; //faster level
+			timeBetweenLevels *= 1.05f; //longer level
+			Player.MOVE_SPEED *= 1.05f; //the player can move faster
 		}
 		
+		//special background colors for certain levels
 		if(currentLevel == 8)
 			background.setColor(Color.WHITE);
 		else if (currentLevel == 9)
 			background.setColor(Color.BLACK);
 		else
-			background.setColorRandom();
+			background.setColorRandom(); //does not set a random color (but it used to)
 		
-		timeUntilLevelStarts+= totalPauseTime;
-		isBetweenLevels = false;
+		timeUntilLevelStarts+= totalPauseTime; //reset pause timer for the next intermission
+		isBetweenLevels = false; //end of intermission
 	}
 	
+	//triggers once per intermission, when it starts
 	private void startPauseTrigger()
 	{
-		timeUntilLevelEnds += timeBetweenLevels;
-		isBetweenLevels = true;
+		timeUntilLevelEnds += timeBetweenLevels; //reset the level timer for the next level
+		isBetweenLevels = true; //start of intermission
 	}
 	
-	
+	//the main update function
+	//delta is the amount of time since the last update
 	public void update(float delta)
 	{		
-		background.update(delta);
+		background.update(delta); //background update (movement and such)
 		
-		if(isBetweenLevels)
+		//TIMERS
+		if(isBetweenLevels) //if there's an intermission...
 		{			
 			timeUntilLevelStarts -= delta;
 			if(timeUntilLevelStarts <= 0)
 			{
-				endPauseTrigger();
+				endPauseTrigger(); //intermission end trigger
 			}
 		}
 		else {
 			timeUntilLevelEnds -= delta;
 			if(timeUntilLevelEnds <= 0 && currentLevel < 9)
 			{
-				startPauseTrigger();
+				startPauseTrigger(); //intermission start trigger (there are no intermissions on level 9
 			}
 		}
 		
+		//LEVEL CALCULATIONS
 		distanceSinceLastPoint+= levelSpeed*delta;
 		
+		//determines what Y-percentage the next point will have
 		if(distanceSinceLastPoint >= distanceBetweenPoints)
 		{
-			distanceSinceLastPoint -= distanceBetweenPoints;
+			distanceSinceLastPoint -= distanceBetweenPoints; //reset distance
 
-			float convertedHalfTW = (tunnelWidth/TriHard.screen_height)/2;
+			float convertedHalfTW = (tunnelWidth/TriHard.screen_height)/2; //conversion for half the current tunnelwidth in a percentage for easy reference and less calculations
+			
 			currentPoint = nextPoint;
-			float deltaValue = -maximumPointDifference + random.nextFloat()*(maximumPointDifference*2);
+			float deltaValue = -maximumPointDifference + random.nextFloat()*(maximumPointDifference*2); //how much the points will differ
+			
 			nextPoint = currentPoint +deltaValue;
 
+			//correctional calculations (0.05f is a buffer from the edges of the game screen). This prevents the next point from ending up outside the screen
 			if(nextPoint < convertedHalfTW+0.05f || nextPoint > (0.95f - convertedHalfTW))
 				nextPoint = currentPoint - deltaValue;
-
-
+			
+			//if the last one doesn't do it, this one will
 			if(nextPoint < convertedHalfTW+0.05f)
 				nextPoint = convertedHalfTW+0.05f;
 			if(nextPoint > (0.95f - convertedHalfTW))
 				nextPoint = 0.95f - convertedHalfTW;
 		}
 
-		float ratio = distanceSinceLastPoint / distanceBetweenPoints;
+		//INDIVIDUAL ROW CALCULATIONS
 		
+		//update for every row
 		for(int i = 0; i < rows.length;i++)
 		{
 			rows[i].X -= levelSpeed*delta;
-			
-			// Anim
 			rows[i].update(delta);
 		}
-		while(rows[lowestRow].X <= -rowHeight)
+		
+		//moves rows that have fully gone from right to left back to the start. LowestRow determines which row in the array is farthest to the left.
+		while(rows[lowestRow].X <= -rowWidth)
 		{
+			float ratio = distanceSinceLastPoint / distanceBetweenPoints;
 			float activePoint = (currentPoint + (ratio * (nextPoint - currentPoint)))*TriHard.screen_height;
 			
-			rows[lowestRow].Renew(activePoint, tunnelWidth, rowHeight, !isBetweenLevels);
+			rows[lowestRow].Renew(activePoint, tunnelWidth, rowWidth, !isBetweenLevels);
 			lowestRow--;
 			if(lowestRow < 0)
 			lowestRow = nrOfRows-1;
 		}
 	}
 	
+	//draw-function
+	//batch allows the drawing of sprites
 	public void draw(SpriteBatch batch)
 	{
-		background.draw(batch);
+		background.draw(batch); //draws the background
 		
-		game.Particles.render(batch, Gdx.graphics.getDeltaTime());
+		game.Particles.render(batch, Gdx.graphics.getDeltaTime()); //draws particles
 		
+		//determines the position of and draws the pausesprite
 		if(isBetweenLevels && currentLevel >= 1)
 		{
-			float xPos = (float)Math.pow(totalPauseTime/2 - timeUntilLevelStarts, 4f)*TriHard.screen_width/2;
+			float xPos = (float)Math.pow(totalPauseTime/2 - timeUntilLevelStarts, 4f)*TriHard.screen_width/2; //results in a form of swooshing effect
 			
 			if(timeUntilLevelStarts > totalPauseTime/2)
 				xPos = TriHard.screen_width/2 + xPos;
@@ -230,15 +247,17 @@ public class Level {
 			pauseSprite.draw(batch);
 		}
 		
+		//drawing the rows
 		for(int i = 0; i < nrOfRows; i++)
 		{
-			if(rows[i].active)
+			if(rows[i].active) //doesn't draw the rows if they aren't collidable (during an intermission, for example)
 			{
+				//flips the sprite twice, once for each side of the tunnel
 				sprite.flip(false, true);
-				sprite.setBounds(rows[i].X, -TriHard.screen_height + rows[i].leftWidth, rowHeight, TriHard.screen_height);
+				sprite.setBounds(rows[i].X, -TriHard.screen_height + rows[i].leftWidth, rowWidth, TriHard.screen_height);
 				sprite.draw(batch);
 				sprite.flip(false, true);
-				sprite.setBounds(rows[i].X, TriHard.screen_height - rows[i].rightWidth, rowHeight, TriHard.screen_height);
+				sprite.setBounds(rows[i].X, TriHard.screen_height - rows[i].rightWidth, rowWidth, TriHard.screen_height);
 				sprite.draw(batch);
 			}
 		}
--- Tri Hard/Tutorial/src/com/trihard/game/Row.java
+++ Tri Hard/Tutorial/src/com/trihard/game/Row.java
@@ -2,11 +2,16 @@ package com.trihard.game;
 
 public class Row {
 
-	public float X;
-	public float leftWidth;
-	public float rightWidth;
-	public boolean active;
+	public float X; //it's X coordinate
+	public float leftWidth; //the current height of the top row (drawn and collided against)
+	public float rightWidth; //the current height of the bottom row (drawn and collided against)
+
+	public float leftTot; //the maximum height of the top row
+	public float rightTot; //the maximum height of the bottom row
 	
+	public boolean active; //if the row is collidable/drawn
+	
+	//constructor
 	public Row(float center, float tunnelWidth, float X, boolean active)
 	{
 		this.active = active;
@@ -18,6 +23,7 @@ public class Row {
 		rightWidth = 0;
 	}
 	
+	//when a row needs new information, this is called (usually when lowestRow needs to be moved)
 	public void Renew(float center, float tunnelWidth, float width, boolean active)
 	{
 		this.active = active;
@@ -29,15 +35,13 @@ public class Row {
 		rightWidth = 0;
 	}
 	
-	public float leftTot;
-	public float rightTot;
-	
+	//update function (all it does is create the "entering/leaving" effect on the rows. Actual movement is handled in Level)
 	public void update(float delta) {
 		
-		float dist = TriHard.screen_width * 1f/8f;
-		float ratio = 0.5f *  ((TriHard.screen_width - X) / dist) + 0.5f;
+		float dist = TriHard.screen_width * 1f/8f; //the distance for when a row is supposed to be fully entered in the level
+		float ratio = 0.5f *  ((TriHard.screen_width - X) / dist) + 0.5f; //starting amount for the incoming rows
 		
-		if(TriHard.screen_width*7f/8 < X) {
+		if(TriHard.screen_width-dist < X) {
 			leftWidth = ratio * leftTot;
 			rightWidth = ratio * rightTot;
 			if(leftWidth > leftTot) {
@@ -46,7 +50,7 @@ public class Row {
 			if(rightWidth > rightTot) {
 				rightWidth = rightTot;
 			}
-		} else if(TriHard.screen_width*1f/8 > X) {
+		} else if(dist > X) {
 			leftWidth -= (TriHard.screen_height*1f * delta);
 			rightWidth -= (TriHard.screen_height*1f * delta);
 		}
